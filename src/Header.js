2ef18432ec8b11523676a26cd1cd3003c06682ff
Anthony <sean.a.protho1@gmail.com>
2020-05-12 10:29:22 -0700

    screw git pages, and 1st challenge

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header(){
+    return (
+        <div>
+            <h1>FEW 1.2 Final Assessment</h1>
+            <h2>Anthony S. Protho</h2>
+        </div>
+    );
+}
+
+export default Header;
